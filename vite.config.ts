add20f96baa2a7b8874ff74d142cce6a88ab95ec
babamovandrej <andrejbabamov@gmail.com>
2025-06-06 10:24:22 +0200

    Dependency setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import precss from 'precss'
+import stylelint from 'vite-plugin-stylelint'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		dts({ tsconfigPath: './tsconfig.app.json' }),
+		cssInjectedByJsPlugin(),
+		stylelint({
+			fix: true,
+			include: ['./src/**/*.css', './src/**/*.pcss'],
+			configFile: './.stylelintrc.json',
+			emitErrorAsWarning: true,
+		}),
+	],
+	css: {
+		postcss: {
+			plugins: [precss()],
+		},
+	},
 })
