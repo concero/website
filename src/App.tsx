5a31263abc67f9cbd3605b982e785ddcd0f52ab7
babamovandrej <andrejbabamov@gmail.com>
2025-06-06 11:32:21 +0200

    Setup home page export

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import '@concero/ui-kit/styles/concero/index.css'
 import './styles/App.css'
+import { Home } from './pages/Home'
 
 function App() {
-	return <></>
+	return (
+		<Home />
+	)
 }
 
 export default App
