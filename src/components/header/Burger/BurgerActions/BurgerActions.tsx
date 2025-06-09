f3f0b2e8672cfd513dd5c28b0783f08e6d8c6e0c
babamovandrej <andrejbabamov@gmail.com>
2025-06-09 12:57:28 +0200

    Add link and mailto to header actions

--- src/components/header/Burger/BurgerActions/BurgerActions.tsx
+++ src/components/header/Burger/BurgerActions/BurgerActions.tsx
@@ -2,9 +2,33 @@ import type { FC } from 'react'
 import { Button } from '@concero/ui-kit'
 import "./BurgerActions.pcss"
 
-export const BurgerActions: FC = () => (
-  <div className="header_burger_actions">
-    <Button variant="primary" size="xl" isFull>Contact Us</Button>
-    <Button variant="secondary_color" size="xl" isFull>Open Testnet</Button>
-  </div>
-)
+export const BurgerActions: FC = () => {
+  const handleContactUs = () => {
+    window.location.href = 'mailto:contact@concero.io'
+  }
+
+  const handleOpenTestnet = () => {
+    window.open('https://app.concero.io', '_blank')
+  }
+
+  return (
+    <div className="header_burger_actions">
+      <Button 
+        variant="primary" 
+        size="xl" 
+        isFull 
+        onClick={handleContactUs}
+      >
+        Contact Us
+      </Button>
+      <Button 
+        variant="secondary_color" 
+        size="xl" 
+        isFull 
+        onClick={handleOpenTestnet}
+      >
+        Open Testnet
+      </Button>
+    </div>
+  )
+}
