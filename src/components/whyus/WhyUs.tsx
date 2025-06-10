bc1a08e44714212d1bf812670779ef0201e30d83
babamovandrej <andrejbabamov@gmail.com>
2025-06-10 12:00:03 +0200

    Add the info widgets to why us section

--- src/components/whyus/WhyUs.tsx
+++ src/components/whyus/WhyUs.tsx
@@ -1,4 +1,8 @@
 import type { FC } from 'react'
+import { InfoWidget } from '../common/InfoWidget/InfoWidget'
+import { LiquidityRequirementIcon } from '@/assets/icons/liquidityReq'
+import { CostIcon } from '@/assets/icons/cost'
+import { TimeIcon } from '@/assets/icons/time'
 import './WhyUs.pcss'
 
 export const WhyUs: FC = (): JSX.Element => {
@@ -6,7 +10,28 @@ export const WhyUs: FC = (): JSX.Element => {
 		<section className="why_us">
 			<span className="why_us_title">Why us?</span>
 			<div className="why_us_content">
-				<div className="why_us_info"></div>
+				<div className="why_us_info">
+					<div className='why_us_info_left'>
+						<InfoWidget
+							title='Integration Time'
+							subtitle='<20min'
+							icon={<TimeIcon />}
+						/>
+						<InfoWidget
+							title='Integration Cost'
+							subtitle='$0'
+							icon={<CostIcon />}
+						/>
+
+					</div>
+					<div className='why_us_info_right'>
+						<InfoWidget
+							title='Liquidity Requirements'
+							subtitle='99.5% less than industry standard '
+							icon={<LiquidityRequirementIcon/>}
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	)
