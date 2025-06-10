b48ce75785253b2c66e737c347450787591c017e
babamovandrej <andrejbabamov@gmail.com>
2025-06-10 09:59:30 +0200

    Add custom media plugin

--- .postcss.config.js
+++ .postcss.config.js
@@ -1,12 +1,15 @@
 import precss from 'precss'
 import autoprefixer from 'autoprefixer'
+import postcssModules from 'postcss-modules'
+import postcssCustomMedia from 'postcss-custom-media'
 
 export default {
-	plugins: [
-		precss(),
-		autoprefixer(),
-		postcssModules({
-			generateScopedName: '[name]__[local]__[hash:base64:5]',
-		}),
-	],
+  plugins: [
+    precss(),
+    autoprefixer(),
+    postcssCustomMedia(),
+    postcssModules({
+      generateScopedName: '[name]__[local]__[hash:base64:5]',
+    }),
+  ],
 }
