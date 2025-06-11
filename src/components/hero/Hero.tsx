b6daed99358315d4d09e0d2d2a25505ccf5c3c9f
babamovandrej <andrejbabamov@gmail.com>
2025-06-11 16:38:59 +0200

    Finish hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,30 +5,34 @@ import { DiscordIcon } from '@/assets/icons/discord'
 import { MediumIcon } from '@/assets/icons/medium'
 import { links } from '@/configuration/links'
 import { SocialIcon } from '../common/SocialIcon/SocialIcon'
-import { useIsTablet, useIsUltrawide } from '@/hooks/useMediaQuery'
+import { useIsMobile, useIsTablet, useIsUltrawide } from '@/hooks/useMediaQuery'
 import './Hero.pcss'
 
-const socials = [
+const SOCIALS = [
 	{
 		icon: <TwitterIcon />,
-		name: 'twitter',
+		name: 'twitter' as const,
 		link: links.twitter,
 	},
 	{
 		icon: <DiscordIcon />,
-		name: 'discord',
+		name: 'discord' as const,
 		link: links.discord,
 	},
 	{
 		icon: <MediumIcon />,
-		name: 'medium',
-		link: links.madium,
+		name: 'medium' as const,
+		link: links.medium,
 	},
 ] as const
 
 export const Hero: FC = (): JSX.Element => {
+	const isMobile = useIsMobile()
 	const isTablet = useIsTablet()
 	const isUltrawide = useIsUltrawide()
+
+	const heroImage = isTablet ? '/Hero/HeroTablet.png' : isMobile ? '/Hero/HeroMobile.svg' : '/Hero/Hero.svg'
+	const buttonSize = isTablet || isUltrawide ? 'xl' : 'l'
 
 	return (
 		<section className="hero">
@@ -42,14 +46,23 @@ export const Hero: FC = (): JSX.Element => {
 					<span className="hero_subtitle">Go anywhere with Concero today</span>
 				</div>
 				<div className="hero_actions">
-					<Button size={isTablet || isUltrawide ? 'xl' : 'l'} variant="secondary_color">
+					<Button size={buttonSize} variant="secondary_color">
 						Contact us
 					</Button>
 					<div className="hero_socials">
-						{socials.map(({ icon, name, link }) => (
+						{SOCIALS.map(({ icon, name, link }) => (
 							<SocialIcon key={name} icon={icon} social={name} link={link} />
 						))}
 					</div>
+				</div>
+				<div className="hero_illustration_container">
+					<img
+						src={heroImage}
+						alt="Hero Illustration"
+						className="hero_illustration"
+						loading="eager"
+						decoding="async"
+					/>
 				</div>
 			</div>
 		</section>
--- src/configuration/links.ts
+++ src/configuration/links.ts
@@ -1,7 +1,7 @@
 export const links = {
 	twitter: 'https://x.com/concero_io',
 	discord: 'https://discord.com/invite/nBMAdxYTZK',
-	madium: 'https://medium.com/@concero',
+	medium: 'https://medium.com/@concero',
 	whitepaper: 'https://concero.io/v2_whitepaper.pdf',
 	documentation: 'https://docs.concero.io/',
 	blog: 'https://blog.concero.io/',
