5c4160768f7eaec118ed1c9e5da8266e9079c208
babamovandrej <andrejbabamov@gmail.com>
2025-10-27 14:07:20 +0100

    Add numbers section

--- src/components/common/DataWidget/DataWidget.tsx
+++ src/components/common/DataWidget/DataWidget.tsx
@@ -1,20 +1,23 @@
-import type { FC, ReactNode } from 'react'
-import './DataWidget.pcss'
+import type { FC, ReactNode, ReactElement } from 'react';
+import { memo } from 'react';
+import './DataWidget.pcss';
 
 type DataWidgetProps = {
-	title: string
-	data: string
-	icon: ReactNode
+    title: string;
+    data: string;
+    icon: ReactNode;
 }
 
-export const DataWidget: FC<DataWidgetProps> = ({ title, data, icon }): JSX.Element => {
-	return (
-		<div className="data_widget">
-			<div className="data_widget_content">
-				<div className="data_widget_icon_container">{icon}</div>
-				<span className="data_widget_title">{title}</span>
-			</div>
-			<span className="data_widget_data">{data}</span>
-		</div>
-	)
-}
+export const DataWidget: FC<DataWidgetProps> = memo(({ title, data, icon }): ReactElement => {
+    return (
+        <article className="data_widget">
+            <div className="data_widget_content">
+                <div className="data_widget_icon_container" aria-hidden="true">
+                    {icon}
+                </div>
+                <h4 className="data_widget_title">{title}</h4>
+            </div>
+            <p className="data_widget_data">{data}</p>
+        </article>
+    );
+});
---
+++ src/components/common/InsightCard/InsightCard.tsx
@@ -0,0 +1,26 @@
+import type { FC, ReactElement } from "react";
+import { memo } from "react";
+import "./InsightCard.pcss";
+
+type InsightCardProps = {
+   title: string;
+   img: string;
+}
+
+export const InsightCard: FC<InsightCardProps> = memo(({ title, img }): ReactElement => {
+    return (
+        <div className="insight_card">
+            <div className="insight_card_content">
+                <img 
+                    src={img} 
+                    alt={title}
+                    loading="lazy"
+                    className="insight_icon"
+                />
+                <span className="insight_title">{title}</span>
+            </div>
+            <div className="insight_divider"/>
+        </div>
+    );
+});
+
