c5e2d9bb7472998e37a8e5adf3ddcda8a3ac18f4
babamovandrej <andrejbabamov@gmail.com>
2025-11-17 10:41:40 +0100

    Finish with stack

--- src/components/overview/Overview.tsx
+++ src/components/overview/Overview.tsx
@@ -3,12 +3,14 @@ import { Hero } from './hero/Hero'
 import { Benefits } from './benefits/Benefits'
 import { Infra } from './infra/Infra'
 import { Schematics } from './schematics/Schematics'
+import { Stack } from './stack/Stack'
 
 export const Overview: FC = (): ReactElement => {
 	return (
 		<>
 			<Hero />
 			<Benefits />
+			<Stack />
 			<Schematics />
 			<Infra />
 		</>
