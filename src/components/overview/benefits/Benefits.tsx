59e71a9828935f449a689443298588eeee257be2
babamovandrej <andrejbabamov@gmail.com>
2025-11-17 14:54:27 +0100

    Optimize the benefits section

--- src/components/overview/benefits/Benefits.tsx
+++ src/components/overview/benefits/Benefits.tsx
@@ -10,57 +10,61 @@ import { UserIcon } from '@/assets/icons/user'
 import { StarIcon } from '@/assets/icons/star'
 import './Benefits.pcss'
 
-const BENEFIT_CARDS_ROW_1 = [
-	{
-		title: 'No vendor-lock',
-		description: 'Retain sovereignty and control with infrastructure that works for you',
-		icon: <StarIcon />,
-	},
-	{
-		title: 'Distribution-first',
-		description: 'Scaling to new chains in under 20 minutes with no added cost',
-		icon: <DistributionIcon />,
-	},
-	{
-		title: 'Permissionless',
-		description: 'Deploy Motherboard to where your users live',
-		icon: <OptimizeIcon />,
-	},
-	{ title: 'Infinite Reach', description: 'Reach any user no matter the chain', icon: <TrustIcon /> },
+const BENEFIT_CARDS_ROWS = [
+  [
+    {
+      title: 'No vendor-lock',
+      description: 'Retain sovereignty and control with infrastructure that works for you',
+      icon: <StarIcon />,
+    },
+    {
+      title: 'Distribution-first',
+      description: 'Scaling to new chains in under 20 minutes with no added cost',
+      icon: <DistributionIcon />,
+    },
+    {
+      title: 'Permissionless',
+      description: 'Deploy Motherboard to where your users live',
+      icon: <OptimizeIcon />,
+    },
+    {
+      title: 'Infinite Reach',
+      description: 'Reach any user no matter the chain',
+      icon: <TrustIcon />,
+    },
+  ],
+  [
+    {
+      title: 'Optimise for you and your users',
+      description: 'Define your interoperability stack according to your demands',
+      icon: <InfinityIcon />,
+    },
+    {
+      title: 'Infinitely upgradable',
+      description: 'Integrate once and upgrade to the latest easily',
+      icon: <ServicingIcon />,
+    },
+    {
+      title: 'Self-Servicing',
+      description: 'Quickly deploy your own modules to gain as much control as you need',
+      icon: <UserIcon />,
+    },
+    {
+      title: 'Trustless',
+      description: 'No one party has control of your transaction',
+      icon: <ReachIcon />,
+    },
+  ],
 ]
 
-const BENEFIT_CARDS_ROW_2 = [
-	{
-		title: 'Optimise for you and your users',
-		description: 'Define your interoperability stack according to your demands',
-		icon: <InfinityIcon />,
-	},
-	{
-		title: 'Infinitely upgradable',
-		description: 'Integrate once and upgrade to the latest easily',
-		icon: <ServicingIcon />,
-	},
-	{
-		title: 'Self-Servicing',
-		description: 'Quickly deploy your own modules to gain as much control as you need',
-		icon: <UserIcon />,
-	},
-	{ title: 'Trustless', description: 'No one party has control of your transaction', icon: <ReachIcon /> },
-]
-
-export const Benefits: FC = (): ReactElement => {
-	return (
-		<div className="overview_benefits">
-			<div className="overview_benefits_cards">
-				{BENEFIT_CARDS_ROW_1.map((card, index) => (
-					<BenefitCard key={index} {...card} />
-				))}
-			</div>
-			<div className="overview_benefits_cards">
-				{BENEFIT_CARDS_ROW_2.map((card, index) => (
-					<BenefitCard key={index} {...card} />
-				))}
-			</div>
-		</div>
-	)
-}
+export const Benefits: FC = (): ReactElement => (
+  <div className="overview_benefits">
+    {BENEFIT_CARDS_ROWS.map((row, rowIndex) => (
+      <div key={rowIndex} className="overview_benefits_cards">
+        {row.map((card, cardIndex) => (
+          <BenefitCard key={cardIndex} {...card} />
+        ))}
+      </div>
+    ))}
+  </div>
+)
