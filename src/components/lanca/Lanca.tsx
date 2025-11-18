0246677c5db73b7cf22389608d6fbcb23f0f8a5e
babamovandrej <andrejbabamov@gmail.com>
2025-11-18 10:58:56 +0100

    Add benefit cards in lanca

--- src/components/lanca/Lanca.tsx
+++ src/components/lanca/Lanca.tsx
@@ -1,12 +1,14 @@
 import type { FC, ReactElement } from 'react'
 import { Hero } from './hero/Hero'
 import { Reach } from './reach/Reach'
+import { Benefits } from './benefits/Benefits'
 
 export const Lanca: FC = (): ReactElement => {
     return (
         <>
             <Hero />
             <Reach />
+            <Benefits />
         </>
     )
 }
