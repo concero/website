8f17eb0af8db2dfab736228f9cbff8ca44300f61
mikhail-concero <mikhail@concero.io>
2025-12-23 16:41:39 +0300

    update action on motherboard

--- src/components/overview/action/Action.tsx
+++ src/components/overview/action/Action.tsx
@@ -2,18 +2,27 @@ import type { FC, ReactElement } from 'react'
 import { Button } from '@concero/ui-kit'
 import './Action.pcss'
 import { useWidthScreen } from '@/hooks/useWidthScreen'
+import { useModalContext } from '@/reducer/modal/modalContext'
+import { links } from '@/configuration/links'
 
 export const Action: FC = (): ReactElement => {
 	const isHdDesktop = useWidthScreen('desktop', 'down')
+	const { dispatch } = useModalContext()
+	const handleStartBuilding = () => {
+		dispatch({ type: 'OPEN_CONTACT' })
+	}
+	const handleDocumentation = () => {
+		window.open(links.documentation, '_blank', undefined)
+	}
 	return (
 		<div className="overview_action">
 			<div className="overview_action_content">
 				<span className="overview_action_title">Build your cross-chain protocol today</span>
 				<div className="overview_action_actions">
-					<Button variant="primary" size={isHdDesktop ? 'l' : 'xl'} isFull>
+					<Button variant="primary" size={isHdDesktop ? 'l' : 'xl'} isFull onClick={handleDocumentation}>
 						Start Building
 					</Button>
-					<Button variant="secondary" size={isHdDesktop ? 'l' : 'xl'} isFull>
+					<Button variant="secondary" size={isHdDesktop ? 'l' : 'xl'} isFull onClick={handleStartBuilding}>
 						Contact Us
 					</Button>
 				</div>
